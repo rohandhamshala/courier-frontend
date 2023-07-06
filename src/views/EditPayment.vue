409924aaf13a4f530944e1692ac7dfc5ec1566f1
Surya2987 <133065224+Surya2987@users.noreply.github.com>
2023-07-06 16:03:45 -0500

    Add Sample pages with title

---
+++ src/views/EditPayment.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+<v-container>
+  <div>
+  <h5>Edit Payment page</h5>
+   <div
+</v-container>
+
+</template>
---
+++ src/views/MyPaymentList.vue
@@ -0,0 +1,10 @@
+
+
+<template>
+<v-container>
+  <div>
+  <h5>My Payment page</h5>
+   <div
+</v-container>
+
+</template>
