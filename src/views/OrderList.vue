d897d2eab9c7eab2aee36372c011e27729cb20b7
Surya2987 <133065224+Surya2987@users.noreply.github.com>
2023-07-07 17:47:47 -0500

    changes in orderList

--- src/views/OrderList.vue
+++ src/views/OrderList.vue
@@ -110,7 +110,7 @@ const deleteOrder = async(id,index) => {
   <div style="margin-top: 20px">
     <div style="display: flex; justify-content: center;">
       <h3>Orders</h3>
-      <a class="btn btn-warning create" :href="[ getDomainUrl() + '/okc-couriers/create-order']" style="margin-left:auto;" v-if="user && user.role_id != 3" >Create Order</a>
+      <a class="btn btn-warning create" :href="[ getDomainUrl() + '/create-order']" style="margin-left:auto;" v-if="user && user.role_id != 3" >Create Order</a>
     </div>
     <br/>
     <div style="display:flex;margin-bottom:10px;">
