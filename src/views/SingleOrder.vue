97299f9ce42e50279737ca8136cc23c22c23d21b
Surya2987 <133065224+Surya2987@users.noreply.github.com>
2023-07-18 12:19:29 -0500

    Add single Order page

---
+++ src/views/SingleOrder.vue
@@ -0,0 +1,251 @@
+<script setup>
+import { onMounted, watch } from "vue";
+import { useRouter } from "vue-router";
+import OrderServices from "../services/OrderServices.js";
+import { ref } from "vue";
+import Spinner from "../components/Spinner.vue";
+import Snackbar from "../components/Snackbar.vue";
+import { updateSnackbar } from "../utils"
+
+
+const order = ref([]);
+const spinner = ref(true);
+const user = ref(null);
+const snackbar = ref({
+  value: false,
+  color: "",
+  text: "",
+});
+const router = useRouter()
+
+onMounted(async () => {
+  user.value = JSON.parse(localStorage.getItem("user"));
+  if(!user.value) {
+    router.push({ name: "login" });
+  }
+  await getOrder();
+  spinner.value = false;
+});
+
+async function getOrder() {
+  await OrderServices.getOrder(router.currentRoute.value.params.id)
+    .then((res) => {
+      order.value = res.data;
+      console.log("order",res.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+const deleteOrder = async() => {
+    await OrderServices.deleteOrder(order.value.id)
+    .then((res) => {
+      snackbar.value = updateSnackbar("Order is deleted successfully!","green")
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value = updateSnackbar(error.response.data.message)
+    });
+}
+const pickedup = async() => {
+    await OrderServices.pickedup(order.value.id)
+    .then((res) => {
+      snackbar.value = updateSnackbar("Order is pickeup successfully!","green")
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value = updateSnackbar(error.response.data.message)
+    });
+}
+const delivered = async() => {
+    await OrderServices.delivered(order.value.id)
+    .then((res) => {
+      snackbar.value = updateSnackbar("Order is delivered successfully!","green")
+    })
+    .catch((error) => {
+      console.log(error);
+      snackbar.value = updateSnackbar(error.response.data.message)
+    });
+}
+</script>
+
+<template>
+<v-container>
+  <div style="margin-top: 20px;margin-left:30px;margin-right:30px;">
+    <div style="display: flex; justify-content: center;">
+      <h3>Order Details</h3>
+    </div>
+    <br/>
+    <Spinner v-if="spinner" />
+     <div class="col-md-12 elevation-4 orders" v-else>
+        <div class="flex">
+            <table>
+                <tr>
+                <th>Order Id</th>
+                <td>{{ order.id }}</td>
+                </tr>
+                <tr>
+                <th>Status</th>
+                <td>{{ order.status }}</td>
+                </tr>
+                <tr>
+                <th>Price</th>
+                <td> ${{ order.price_for_order }}</td>
+                </tr>
+                <tr>
+                <th>Distance</th>
+                <td> {{ order.distance }} miles</td>
+                </tr>
+                 <tr>
+                <th>Price for Delivery Boy</th>
+                <td> ${{ order.price_for_delivery_boy }}</td>
+                </tr>
+                <tr v-if="order.delivery_boy_bonus">
+                <th>Delivery Boy Bonus</th>
+                <td> ${{ order.delivery_boy_bonus }}</td>
+                </tr>
+                <tr>
+                <th>Expected Time for Delivery </th>
+                <td> {{ order.minimum_time }} Minutes</td>
+                </tr>
+                <tr>
+                 <th>Created At</th>
+                <td> {{ order.createdAt }} </td>
+                </tr>
+                <tr v-if="order.pickedup_at" >
+                 <th>Picked up At</th>
+                <td> {{ order.pickedup_at }} </td>
+                </tr>
+                <tr v-if="order.delivered_at" >
+                 <th>Delivered At</th>
+                <td> {{ order.delivered_at }} </td>
+                </tr>
+                <tr v-if="order.delivered_in_time" >
+                 <th>Delivered in time</th>
+                <td> {{ order.delivered_in_time }} </td>
+                </tr>
+                 <tr v-if="order.pickup_customer" >
+                 <th>Pickup Details</th>
+                <td> 
+                    <p> Name - {{ order.pickup_customer.first_name}} {{ order.pickup_customer.last_name}} <br/>
+                     Email - {{ order.pickup_customer.email}} <br/>
+                     Mobile - {{ order.pickup_customer.mobile}} <br/>
+                     Address - {{ order.pickup_customer.address}} <br/>
+                     Apt Number - {{ order.pickup_customer.apartment_number}} </p>
+                </td>
+                </tr>
+                <tr v-if="order.delivery_customer">
+                 <th>Delivery Details</th>
+                <td> 
+                    <p> Name - {{ order.delivery_customer.first_name}} {{ order.delivery_customer.last_name}} <br/>
+                     Email - {{ order.delivery_customer.email}} <br/>
+                     Mobile - {{ order.delivery_customer.mobile}} <br/>
+                     Address - {{ order.delivery_customer.address}} <br/>
+                     Apt Number - {{ order.delivery_customer.apartment_number}} </p>
+                </td>
+                </tr>
+                <tr>
+                <th v-if="order.clerk_details">Clerk Details</th>
+                <td> 
+                    <p> Name - {{ order.clerk_details.firstName}} {{ order.clerk_details.lastName}} <br/>
+                     Email - {{ order.clerk_details.email}} <br/>
+                     Mobile - {{ order.clerk_details.mobile}} <br/>
+                    </p>
+                </td>
+                </tr>
+                <tr v-if="order.delivery_boy_details">
+                <th>Delivery Boy Details</th>
+                <td> 
+                    <p> Name - {{ order.delivery_boy_details.firstName}} {{ order.delivery_boy_details.lastName}} <br/>
+                     Email - {{ order.delivery_boy_details.email}} <br/>
+                     Mobile - {{ order.delivery_boy_details.mobile}} <br/>
+                    </p>
+                </td>
+                </tr>
+                <tr v-if="user.role_id =='3' && !order.delivered_at ">
+                    <th>Actions</th>
+                    <td>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-secondary edit" v-if="!order.pickedup_at" @click="pickedup()">Picked Up</button>
+                        <button type="button" class="btn btn-secondary edit" v-if="!order.delivered_at" @click="delivered()">Delivered</button>
+                        </div> 
+                    </td>
+                </tr>
+
+                
+            </table>
+
+        </div>
+    </div>
+  </div>
+  <Snackbar :snackbar="snackbar"/>
+  <br/>
+</v-container>
+
+</template>
+
+<style scoped>
+
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+table {
+    width: 80%;
+}
+td {
+    width: 80%;
+    padding: 10px;
+}
+th {
+    padding-left: 10px;
+}
+.flex {
+    display: flex;
+    justify-content: space-around;
+}
+.orders {
+    background-color: white;
+}
+.orders {
+  padding: 30px;
+}
+.order {
+  margin-top: 30px;
+  width:100%;
+  border-bottom: 1px solid black;
+  padding:15px;
+  margin-left: 20px;
+}
+.btn-danger {
+  margin-left: 20px;
+}
+.users{
+  margin-top: 20px;
+  margin-bottom: 20px;
+}
+.underline {
+  border-bottom: 2px solid black;
+  width: 40%;
+  margin-bottom: 10px;
+}
+.create {
+    background-color: #1877f2 ;
+    margin-left: 20px;
+    padding-top: -10px;
+    color: white;
+}
+.edit {
+  background-color: #198754;
+  color: white;
+}
+.delete {
+  background-color: #dc3545;
+  color: white;
+}
+.search {
+  margin-left: 70%;
+  width: 30%;
+}
+</style>
